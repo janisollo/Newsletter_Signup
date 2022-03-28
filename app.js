735f201581ccd0aecd28c51a65d30ca15275198b
janisollo <sollo.jani@gmail.com>
2022-03-28 12:11:44 +0700

    middle of the api solution

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const request = require("request")
+const https = require("https");
+const { options } = require("request");
 
 const app = express();
 app.use(express.static("public"))
@@ -12,15 +14,38 @@ app.get("/", function (req, res) {
 })
 
 app.post("/", function (req, res) {
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_adress: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+    const jsonData = JSON.stringify(data);
+    const url = "https://usX.api.mailchimp.com/3.0/lists/7f042ac910"
+
+    https.request(url, options, function (response) {
+
+    })
+});
 
 
-    console.log(firstName, lastName, email);
-})
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000");
 })
 
+
+
+//API Key a020ee51898c233faa49485c4d7db556
+
+// List id 7f042ac910
